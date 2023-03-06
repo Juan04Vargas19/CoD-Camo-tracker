ac2da31faf931f6ec3db7764312defefb15681b9
Juan04Vargas19 <juanda98vargas@gmail.com>
2023-03-06 14:09:26 -0500

    Interactividad agregada

--- js/main.js
+++ js/main.js
@@ -13,7 +13,30 @@ const a = document.querySelectorAll('.a')
 const b = document.querySelectorAll('.b')
 const arr = document.querySelectorAll('.arrow')
 
-const inputsBase = document.getElementsByClassName('inputs-base')
+const buttonsBase = document.getElementsByClassName('button-base')
+const buttonsOro = document.getElementsByClassName('button-oro')
+const buttonsPlat = document.getElementsByClassName('button-plat')
+const buttonsPoli = document.getElementsByClassName('button-poli')
+
+const armasImg = document.getElementsByClassName('fusiles-asalto-elemento-img')
+console.log(armasImg)
+
+let armasBase = []
+let armasOro = []
+let armasPlat = []
+let armasPoli = []
+let buttonsPlatAr = []
+let buttonsPlatBr = []
+let buttonsPlatSmg = []
+let buttonsPlatShotgun = []
+let buttonsPlatLmg = []
+let buttonsPlatMarkR = []
+let buttonsPlatSnpr = []
+let buttonsPlatPistol = []
+let buttonsPlatRckt = []
+let buttonsPlatMelee = []
+let buttonsPoliArr = []
+
 
 let intentos = 0
 //Extrae las armas en las que ya se completaron todos 4 los desafíos base, y por ende ya tienen el desafío de oro disponible
@@ -38,16 +61,15 @@ armas.forEach((arma)=>{
         const inputBase = document.createElement('div')
 
         fusilesAsalto.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         fusilesAsaltoImg.innerHTML = `<img src='${arma.img}' class="fusiles-asalto-elemento-img">`
 
         fusilesAsalto.className = 'fusiles-asalto-elemento'
-        fusilesAsaltoImg.className = 'fusiles-asalto-elemento-img'
 
         sectionFusilesAsalto.appendChild(fusilesAsalto)
         fusilesAsalto.appendChild(fusilesAsaltoImg)
@@ -57,34 +79,32 @@ armas.forEach((arma)=>{
         const fusilesCombateImg = document.createElement('div')
 
         fusilesCombate.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         fusilesCombateImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         fusilesCombate.className = 'fusiles-asalto-elemento'
-        fusilesCombateImg.className = 'fusiles-asalto-elemento-img'
 
         sectionFusilesCombate.appendChild(fusilesCombate)
         fusilesCombate.appendChild(fusilesCombateImg)
-    }else if(arma.tipo === 'SMG'){
+    } else if(arma.tipo === 'SMG'){
         const subfusiles = document.createElement('li')
         const subfusilesImg = document.createElement('div')
 
         subfusiles.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         subfusilesImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         subfusiles.className = 'fusiles-asalto-elemento'
-        subfusilesImg.className = 'fusiles-asalto-elemento-img'
 
         sectionSubfusiles.appendChild(subfusiles)
         subfusiles.appendChild(subfusilesImg)
@@ -93,16 +113,15 @@ armas.forEach((arma)=>{
         const escopetaImg = document.createElement('div')
 
         escopetas.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         escopetaImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         escopetas.className = 'fusiles-asalto-elemento'
-        escopetaImg.className = 'fusiles-asalto-elemento-img'
 
         sectionEscopetas.appendChild(escopetas)
         escopetas.appendChild(escopetaImg)
@@ -111,16 +130,15 @@ armas.forEach((arma)=>{
         const ametralladoraImg = document.createElement('div')
 
         ametralladoras.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         ametralladoraImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         ametralladoras.className = 'fusiles-asalto-elemento'
-        ametralladoraImg.className = 'fusiles-asalto-elemento-img'
 
         sectionAmetralladoras.appendChild(ametralladoras)
         ametralladoras.appendChild(ametralladoraImg)
@@ -129,16 +147,15 @@ armas.forEach((arma)=>{
         const fusilesTactImg = document.createElement('div')
 
         fusilesTact.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         fusilesTactImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         fusilesTact.className = 'fusiles-asalto-elemento'
-        fusilesTactImg.className = 'fusiles-asalto-elemento-img'
 
         sectionFusilesTacticos.appendChild(fusilesTact)
         fusilesTact.appendChild(fusilesTactImg)
@@ -147,16 +164,15 @@ armas.forEach((arma)=>{
         const sniperImg = document.createElement('div')
 
         snipers.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         sniperImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         snipers.className = 'fusiles-asalto-elemento'
-        sniperImg.className = 'fusiles-asalto-elemento-img'
 
         sectionSnipers.appendChild(snipers)
         snipers.appendChild(sniperImg)
@@ -165,16 +181,15 @@ armas.forEach((arma)=>{
         const pistolasImg = document.createElement('div')
 
         pistolas.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         pistolasImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         pistolas.className = 'fusiles-asalto-elemento'
-        pistolasImg.className = 'fusiles-asalto-elemento-img'
 
         sectionPistolas.appendChild(pistolas)
         pistolas.appendChild(pistolasImg)
@@ -183,16 +198,15 @@ armas.forEach((arma)=>{
         const launchersImg = document.createElement('div')
 
         launchers.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         launchersImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         launchers.className = 'fusiles-asalto-elemento'
-        launchersImg.className = 'fusiles-asalto-elemento-img'
 
         sectionLaunchers.appendChild(launchers)
         launchers.appendChild(launchersImg)
@@ -201,16 +215,15 @@ armas.forEach((arma)=>{
         const meleeImg = document.createElement('div')
 
         melee.innerHTML = `
-        <p class="fusiles-asalto-elemento-nombre">${arma.nombre}</p>
-        <input type="checkbox" name="base" class="inputs-base">
-        <input type="checkbox" name="base" class="inputs-oro">
-        <input type="checkbox" name="base" class="inputs-plat">
-        <input type="checkbox" name="base" class="inputs-poli">
+        <p class="armas-elemento-nombre">${arma.nombre}</p>
+        <button class="button-base" data-arma="${arma.nombre}"></button>
+        <button class="button-oro" data-arma="${arma.nombre}" disabled></button>
+        <button class="button-plat" data-arma="${arma.nombre}" categoria="${arma.tipo}" disabled></button>
+        <button class="button-poli" data-arma="${arma.nombre}" disabled></button>
         `
         meleeImg.innerHTML = `<img src='${arma.img}' class='fusiles-asalto-elemento-img'>`
 
         melee.className = 'fusiles-asalto-elemento'
-        meleeImg.className = 'fusiles-asalto-elemento-img'
 
         sectionMelee.appendChild(melee)
         melee.appendChild(meleeImg)
@@ -237,30 +250,152 @@ const pistolArr = armas.filter(arma => arma.tipo === 'pistol')
 const launcherArr = armas.filter(arma => arma.tipo === 'rckt')
 const meleeArr = armas.filter(arma => arma.tipo === 'melee')
 
-preguntarBase()
-
-function preguntarBase(){
-    for(let i = 0; i < armas.length; i++){
-        let arma = armas[i]
-        let inputBase = inputsBase[i]
-        
-        inputBase.addEventListener('click', ()=>{
-            arma.desafiosBase = true
-        })
-        
-    }
-    console.log(fusilesAsaltoArr)
-
-    
-    //Se crea un arreglo que almacena las armas en las que se completaron los 4 camuflajes base y por ende ya tienen disponible el desafío de Oro
-    armasOroDisponible = armas.filter(arma => arma.desafiosBase === true)
-    console.log(armasOroDisponible)
-    if(armasOroDisponible.length === 0){
-        // alert('Recuerda que debes desbloquear los 4 camuflajes base de cada arma para desbloquear los desafíos de Oro')
-    } else{
-        // preguntarOro()
-    }
+for(let i = 0; i < buttonsBase.length; i++) {
+    buttonsBase[i].addEventListener('click', () => {
+        buttonsBase[i].classList.toggle('button-base-clicked')
+        const nombreArma = buttonsBase[i].getAttribute('data-arma')
+        const indexArma = armas.findIndex(arma => arma.nombre === nombreArma)
+        armas[indexArma].desafiosBase = true
+        buttonsOro[indexArma].disabled = false
+        console.log(armas)
+        armasBase = armas.filter(arma => arma.desafiosBase === true)
+        console.log (armasBase)
+    })
 }
+
+
+for(let i = 0; i < buttonsOro.length; i++) {
+    buttonsOro[i].addEventListener('click', () => {
+        buttonsOro[i].classList.toggle('button-oro-clicked')
+        const nombreArma = buttonsOro[i].getAttribute('data-arma')
+        const indexArma = armas.findIndex(arma => arma.nombre === nombreArma)
+        armas[indexArma].oro = true
+        armasImg[indexArma].setAttribute('src', `${armas[indexArma].imgOro}`)
+        console.log(armas)
+        armasOro = armas.filter(arma => arma.oro === true)
+        console.log (armasOro)
+        const buttonsPlatSec = buttonsPlat[i].getAttribute('categoria')
+        if(buttonsPlatSec === 'AR'){
+            buttonsPlatAr.push(buttonsPlat[i])
+        } else if(buttonsPlatSec === 'BR'){
+            buttonsPlatBr.push(buttonsPlat[i])
+        } else if(buttonsPlatSec === 'SMG'){
+            buttonsPlatSmg.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'shotgun'){
+            buttonsPlatShotgun.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'LMG'){
+            buttonsPlatLmg.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'markR'){
+            buttonsPlatMarkR.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'snpr'){
+            buttonsPlatSnpr.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'pistol'){
+            buttonsPlatPistol.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'rckt'){
+            buttonsPlatRckt.push(buttonsPlat[i])
+        }else if(buttonsPlatSec === 'melee'){
+            buttonsPlatMelee.push(buttonsPlat[i])
+        }
+        console.log(buttonsPlatAr)
+
+        if(buttonsPlatAr.length >= 8){
+            for(let j = 0; j < buttonsPlatAr.length; j++)
+            buttonsPlatAr[j].disabled = false
+        }
+
+        if(buttonsPlatBr.length >= 4){
+            for(let j = 0; j < buttonsPlatBr.length; j++)
+            buttonsPlatBr[j].disabled = false
+        }
+
+        if(buttonsPlatSmg.length >= 8){
+            for(let j = 0; j < buttonsPlatSmg.length; j++)
+            buttonsPlatSmg[j].disabled = false
+        }
+
+        if(buttonsPlatShotgun.length >= 4){
+            for(let j = 0; j < buttonsPlatShotgun.length; j++)
+            buttonsPlatShotgun[j].disabled = false
+        }
+
+        if(buttonsPlatLmg.length >= 6){
+            for(let j = 0; j < buttonsPlatLmg.length; j++)
+            buttonsPlatLmg[j].disabled = false
+        }
+
+        if(buttonsPlatMarkR.length >= 6){
+            for(let j = 0; j < buttonsPlatMarkR.length; j++)
+            buttonsPlatMarkR[j].disabled = false
+        }
+
+        if(buttonsPlatSnpr.length >= 4){
+            for(let j = 0; j < buttonsPlatSnpr.length; j++)
+            buttonsPlatSnpr[j].disabled = false
+        }
+
+        if(buttonsPlatPistol.length >= 5){
+            for(let j = 0; j < buttonsPlatPistol.length; j++)
+            buttonsPlatPistol[j].disabled = false
+        }
+
+        if(buttonsPlatRckt.length >= 4){
+            for(let j = 0; j < buttonsPlatRckt.length; j++)
+            buttonsPlatRckt[j].disabled = false
+        }
+
+        if(buttonsPlatMelee.length >= 2){
+            for(let j = 0; j < buttonsPlatMelee.length; j++)
+            buttonsPlatMelee[j].disabled = false
+        }
+    })
+}
+
+for(let i = 0; i < buttonsPlat.length; i++) {
+    buttonsPlat[i].addEventListener('click', () => {
+        buttonsPlat[i].classList.toggle('button-plat-clicked')
+        const nombreArma = buttonsPlat[i].getAttribute('data-arma')
+        const indexArma = armas.findIndex(arma => arma.nombre === nombreArma)
+        armas[indexArma].platino = true
+        console.log(armas)
+        armasPlat = armas.filter(arma => arma.platino === true)
+        console.log (armasPlat)
+        armasImg[indexArma].setAttribute('src', `${armas[indexArma].imgPlat}`)
+
+        buttonsPoliArr.push(buttonsPoli[i])
+
+        console.log(buttonsPoliArr)
+        
+        if(buttonsPoliArr.length >= 51){
+            for(let j = 0; j < buttonsPoliArr.length; j++)
+            buttonsPoliArr[j].disabled = false
+        }
+    })
+}
+
+for(let i = 0; i < buttonsPlat.length; i++) {
+    buttonsPoli[i].addEventListener('click', () => {
+        buttonsPoli[i].classList.toggle('button-plat-clicked')
+        const nombreArma = buttonsPoli[i].getAttribute('data-arma')
+        const indexArma = armas.findIndex(arma => arma.nombre === nombreArma)
+        armas[indexArma].poli = true
+        console.log(armas)
+        armasPoli = armas.filter(arma => arma.poli === true)
+        console.log (armasPoli)
+        armasImg[indexArma].setAttribute('src', `${armas[indexArma].imgPoli}`)
+        
+        if(armasPoli.length >= 51){
+            for(let j = 0; j < buttonsPoliArr.length; j++)
+            armasImg[j].setAttribute('src', `${armas[j].imgOrion}`)
+        }
+    })
+}
+
+
+
+// function test(){
+//     const armasConDesafiosBase = filtrarPorDesafiosBase(armas)
+//     console.log(armasConDesafiosBase)
+// }
 
 //función para preguntar nombre de usuario o correo al user
 // function preguntarUsuario(){
